c28744ef6d28ded1a271463848abc92855f864b4
pl9ed <pl9ed@virginia.edu>
2020-11-24 01:02:07 -0500

    websocket skeleton

---
+++ src/main/java/tubalubback/controllers/MusicController.java
@@ -0,0 +1,25 @@
+package tubalubback.controllers;
+
+import models.MusicSyncInfo;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MusicController {
+
+    @MessageMapping("/update")
+    @SendTo("/topic/music")
+    public MusicSyncInfo playPause(MusicSyncInfo input) {
+        return input;
+    }
+
+    @MessageMapping("/test")
+    @SendTo("/topic/test")
+    public String test(String input) {
+        System.out.println(input);
+        return input;
+    }
+}
