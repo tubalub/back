1c4c9e0909753ceebde947145b6a4ab9aba1c9cf
Connor Cheong <connor.cheong@gmail.com>
2020-11-23 22:24:10 -0500

    sort of syncs, trying to test the extent of syncing

--- src/main/java/tubalubback/TubalubBackApplication.java
+++ src/main/java/tubalubback/TubalubBackApplication.java
@@ -1,13 +1,22 @@
 package tubalubback;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class TubalubBackApplication {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(TubalubBackApplication.class, args);
 	}
-
 }
--- src/main/java/tubalubback/controller/MusicController.java
+++ src/main/java/tubalubback/controller/MusicController.java
@@ -1,13 +1,24 @@
 package tubalubback.controller;
 
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.SequenceInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +26,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import tubalubback.TubalubBackApplication;
+
 @RestController
 @RequestMapping("/music")
 public class MusicController {
+	public static DataInputStream dataStream;
+	public static int pos = -1;
+	public static List<String> files = new ArrayList<>();
+	public static List<ServletOutputStream> streams = new ArrayList<>();
+	
+	public void startStreaming() throws IOException {
+		System.out.println("starting stream");
+		if(files.isEmpty()) {
+			files.add("D:\\Home\\Music\\2AM\\1집 Saint o'clock\\13 이 노래.mp3");
+			files.add("D:\\Home\\Music\\Taylor Swift\\Love Story\\00 Love Story.mp3");
+			files.add("D:\\Home\\Music\\Tablo\\열꽃_ Part 1\\02 나쁘다(feat_ 진실).mp3");
+		}
+		if (++pos >= files.size()) {
+			pos=0;
+		}
+		dataStream = new DataInputStream(new FileInputStream(files.get(pos)));
+	}
+	
+	@RequestMapping("/stream/next") 
+	public ResponseEntity<Void> nextSong(){
+		System.out.println("next song");
+		streams.forEach(stream -> {
+			try {
+				stream.flush();
+				stream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		return new ResponseEntity<>(HttpStatus.OK);
+		
+	}
+
 	
 	@GetMapping("/stream")
-	public ResponseEntity<StreamingResponseBody> streamMusic(HttpServletResponse response) {
-		final File testFile = new File("D:\\Home\\Music\\2AM\\1집 Saint o'clock\\13 이 노래.mp3");
+	public ResponseEntity<StreamingResponseBody> streamMusic(HttpServletResponse response) throws IOException {
+		if(dataStream==null) {
+			startStreaming();
+		}
 		StreamingResponseBody stream = out ->{
-			final InputStream output = new FileInputStream(testFile);
-			output.transferTo(response.getOutputStream());
+			System.out.println("transfering stream");
+			if(dataStream.available()==0) {
+				startStreaming();
+			}
+			IOUtils.copyLarge(dataStream, response.getOutputStream());
+			streams.add(response.getOutputStream());
 		};
 		return new ResponseEntity<>(stream, HttpStatus.OK);
 	}
