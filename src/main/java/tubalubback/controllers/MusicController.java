86813ca4c632ee48ac09907d01bd5001bd5c625a
pl9ed <pl9ed@virginia.edu>
2020-11-25 15:39:47 -0500

    Files in S3 are now deleted once all users have disconnected

--- src/main/java/tubalubback/controllers/MusicController.java
+++ src/main/java/tubalubback/controllers/MusicController.java
@@ -1,28 +1,31 @@
 package tubalubback.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import tubalubback.models.MusicSyncInfo;
+import tubalubback.services.SyncService;
 
 @Controller
 @CrossOrigin
 public class MusicController {
 
+    @Autowired
+    private SyncService syncService;
+
     @MessageMapping("/update")
     @SendTo("/topic/music")
     public MusicSyncInfo playPause(MusicSyncInfo input) {
         System.out.println(input.getTime());
+        syncService.syncUpdate = input;
         return input;
     }
 
-//    @MessageMapping("/test")
-//    @SendTo("/topic/music")
-//    public String test(String input) {
-//        System.out.println(input);
-//        return input;
-//    }
+
 
 }
