3921ec7c0cefa0f8a2b4e1620339fc8d982ff6cd
pl9ed <pl9ed@virginia.edu>
2020-12-01 14:15:01 -0500

    Refactored user list to also use websockets, moved constant endpoint strings used across classes to SyncUtils

--- src/main/java/tubalubback/services/SyncService.java
+++ src/main/java/tubalubback/services/SyncService.java
@@ -1,42 +1,84 @@
 package tubalubback.services;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import tubalubback.controllers.UserController;
 import tubalubback.models.MusicSyncInfo;
 import tubalubback.utils.SyncUtils;
 
+import java.security.Principal;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @Log4j2
 public class SyncService {
 
-    private AtomicInteger users = new AtomicInteger(0);
-
+    // music sync
     public static long currentSongStartTime = -1;
     private static MusicSyncInfo syncUpdate = new MusicSyncInfo();
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public static Set<String> userSet = Collections.synchronizedSet(new HashSet<>());
+    // sessionID key, username values
+    public final static Map<String, String> usernames = new HashMap<>();
+
+    @Autowired
+    private SimpMessagingTemplate simp;
 
     @EventListener(SessionConnectEvent.class)
     public void webSocketConnected(SessionConnectEvent event) {
-        users.addAndGet(1);
-        log.info("New user connected. Currently {} users", users);
+        Message<byte[]> msg = event.getMessage();
+        String username = getUsername(msg);
+        String sessionID = (String) event.getMessage().getHeaders().get("simpSessionId");
+        usernames.put(sessionID, username);
+
+        simp.convertAndSend("/topic/users", usernames.values());
+
+        log.info("{} connected. Currently {} users", username, usernames.size());
     }
 
     @EventListener(SessionDisconnectEvent.class)
     public void webSocketDisconnected(SessionDisconnectEvent event) {
-        users.addAndGet(-1);
-        log.info("User disconnected. Currently {} users", users);
+        Message<byte[]> msg = event.getMessage();
+        String sessionID = event.getSessionId();
+        String username = usernames.get(sessionID);
+        usernames.remove(sessionID);
 
+        simp.convertAndSend("/topic/users", usernames.values());
+
+        log.info("{} disconnected. Currently {} users", username, usernames.size());
+
+        if (usernames.size() < 1) {
+            clearSongLists();
+        }
+    }
+
+    private String getUsername(Message<byte[]> msg) {
+        Principal princ = (Principal) msg.getHeaders().get("simpUser");
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(msg);
+        List<String> nativeHeaders = sha.getNativeHeader("username");
+        String username = "default";
+        if (nativeHeaders != null) {
+            username = nativeHeaders.get(0);
+        } else {
+            if (princ != null) {
+                username = princ.getName();
+            }
+        }
+        return username;
+    }
+
+    private void clearSongLists() {
+        log.info("Empty lobby. Clearing song lists...");
         synchronized(SyncUtils.SYNC_STRING) {
-            if (users.get() < 1) {
+            if (usernames.size() < 1) {
                 for (String url : syncUpdate.getSongQ()) {
                     if (url.contains("s3.amazonaws")) {
                         log.info("Deleting: {}", url);
