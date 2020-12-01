57c659bec525422cb828d167c3d2906312d68c77
pl9ed <pl9ed@virginia.edu>
2020-12-01 17:45:04 -0500

    S3 deletes are now done through SDK rather than http requests

--- src/main/java/tubalubback/services/S3Service.java
+++ src/main/java/tubalubback/services/S3Service.java
@@ -3,7 +3,6 @@ package tubalubback.services;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
@@ -18,26 +17,14 @@ public class S3Service {
 
     public static final String BUCKET_NAME = "tubalub";
     public static final Region S3_REGION = Region.US_EAST_1;
-    public static final String BUCKET_PATH = "/";
+    public static final String BUCKET_PATH = "uploads/";
 
     private static final S3Client s3 = S3Client.builder().region(S3_REGION).credentialsProvider(ProfileCredentialsProvider.create()).build();
     private static final S3Presigner presigner = S3Presigner.builder().region(S3_REGION).credentialsProvider(ProfileCredentialsProvider.create()).build();
 
-    public String putObject(String objKey, byte[] file) {
-        try {
-            PutObjectResponse response = s3.putObject(PutObjectRequest.builder().bucket(BUCKET_NAME).key(BUCKET_PATH+objKey).build(), RequestBody.fromBytes(file));
-            GetUrlRequest urlRequest = GetUrlRequest.builder().bucket(BUCKET_NAME).key(BUCKET_PATH+objKey).region(S3_REGION).build();
-
-            return s3.utilities().getUrl(urlRequest).toExternalForm();
-        } catch (S3Exception e) {
-            // TODO log
-            return null;
-        }
-    }
-
     public boolean deleteObject(String objKey) {
         try {
-            DeleteObjectRequest req = DeleteObjectRequest.builder().key(BUCKET_PATH+objKey).bucket(BUCKET_NAME).build();
+            DeleteObjectRequest req = DeleteObjectRequest.builder().bucket(BUCKET_NAME).key(BUCKET_PATH+objKey).build();
             DeleteObjectResponse resp = s3.deleteObject(req);
             if (resp.sdkHttpResponse().statusCode() / 100 == 2) {
                 return true;
@@ -48,12 +35,16 @@ public class S3Service {
         return false;
     }
 
+    public boolean deleteFromURL(String url) {
+        String filename = url.substring(url.lastIndexOf("/")+1);
+        return deleteObject(filename);
+    }
+
     public String presignPutUrl(String filename) {
         PresignedPutObjectRequest putReq = presigner.presignPutObject(r -> r.signatureDuration(Duration.ofMinutes(2))
-                .putObjectRequest(por -> por.bucket(BUCKET_NAME).key("uploads/"+filename)));
+                .putObjectRequest(por -> por.bucket(BUCKET_NAME).key(BUCKET_PATH+filename)));
         log.info("Presigned URL generated: {}", putReq.url().toString());
         return putReq.url().toString();
     }
-
 
 }
